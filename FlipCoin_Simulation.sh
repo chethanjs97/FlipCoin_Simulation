5e9965835fb300718145e63fd6b552c005d54255
chethanjs97 <chethanjschethu@gmail.com>
2020-10-11 01:40:15 +0530

    Added : Shows its Win or Tie and if win then who won and by how much

--- FlipCoin_Simulation.sh
+++ FlipCoin_Simulation.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -x
 
-read -p "Enter The Number of Times Coin to be Flipped " num
+read -p "Enter The Number of Times coin to be Flipped " num
 head=1
 tail=0
 headcount=0
 tailcount=0
-for ((i=0;i<=$num;i++)) 
+while [ $headcount -lt 21 -a $tailcount -lt 21 ] 
 do
 	
 	randomcheck=$((RANDOM%2))
@@ -20,5 +20,19 @@ do
 
 esac
 done
-echo $headcount
-echo $tailcount
+if [ $headcount -ge 21 ]
+then
+	echo "head wins"
+	diff=$(($headcount-$tailcount)) 
+elif [ $tailcount -ge 21 ]
+then
+	echo "tail wins "
+	diff=$(($tailcount-$headcount))
+else
+	echo "no one wins "
+fi
+
+echo "difference is "$diff
+echo "head count is " $headcount
+
+echo "tail count is "$tailcount
